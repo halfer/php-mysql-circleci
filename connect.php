9ac271341e254783ce3e1a3149ea07bdbc454890
Jon Hinks <projects.ws@jondh.me.uk>
2018-03-07 23:34:32 +0000

    Non-zero exit code on failure

--- connect.php
+++ connect.php
@@ -8,9 +8,9 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
-    }
+}
 catch(PDOException $e)
-    {
+{
     echo "Connection failed: " . $e->getMessage();
-    }
-
+    die(1);
+}
